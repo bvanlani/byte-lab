9f221a8f03ebe64df9d6be7b666761210ec79b7e
bvanlani <ben.vanlaningham@gmail.com>
2025-10-01 13:01:02 -0500

    Initialization commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Nav from './componenets/Nav.jsx'
+import Footer from './componenets/Footer.jsx'
+import Header from './componenets/Header.jsx'
+import IconGrid from './componenets/IconGrid.jsx'
+import Biography from './componenets/Biography.jsx'
+
+import './css/styles.css'
+
+function App() {  
+
+  return (
+    <body>
+      <Nav />
+      <Header />
+      <IconGrid />
+      <Biography />
+      <Footer />
+    </body>
+  )
+}
+
+export default App
---
+++ src/componenets/Nav.jsx
@@ -0,0 +1,10 @@
+export default function Nav(){
+    return(
+        <nav className="navbar bg-light static-top">
+            <div className="container">
+                <a className="navbar-brand" href="#!">Byte Lab</a>
+                <a className="btn btn-primary" href="#signup">Sign Up</a>
+            </div>
+        </nav>
+    )
+}
