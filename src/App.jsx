37ecde6b3cc35f9b1b691c2d05cb5f8a3420f328
bvanlani <ben.vanlaningham@gmail.com>
2025-10-02 08:51:33 -0500

    Quick commit b4 I go to calc

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <BrowserRouter basename="/byte-lab">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
   )
