7c0f5a1a97dfd74309bb24826da780fee44a9e38
Ben <ben.vanlaningham@gmail.com>
2025-10-03 10:42:48 -0500

    added scroll chaining for monaco

--- src/componenets/Compiler.jsx
+++ src/componenets/Compiler.jsx
@@ -1,21 +1,26 @@
-import Editor from '@monaco-editor/react';
+import React, { useEffect, useRef } from "react";
+import { Editor } from "@monaco-editor/react";
 
 export default function Compiler() {
+
   return (
     <section className="vh-100 d-flex flex-column">
       <div className="container bg-dark text-light text-center py-4">
         <h1>The Lab</h1>
       </div>
 
-      <div className="container flex-grow-1 d-flex bg-dark">
-          <Editor
-            height="100%"
-            defaultLanguage="javascript"
-            defaultValue="// Start coding here..."
-            theme="vs-dark"
-          />
+      <div className="container flex-grow-1 d-flex bg-dark text-light">
+        <Editor
+          height="100%"
+          defaultLanguage="javascript"
+          defaultValue={`// Start coding here...`}
+          theme="vs-dark"
+          options={{
+          scrollbar: {
+            alwaysConsumeMouseWheel: false
+          }}}
+        />
       </div>
     </section>
   );
 }
-
