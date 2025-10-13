702805254c74f891e3a4ebafbd888dcb3d65c8c5
cmoney52 <149429981+cmoney52@users.noreply.github.com>
2025-10-13 12:39:30 -0500

    FINALLY
    Finally go the portrait pictures put into the biography section of the website. Only waiting for Bens.

--- src/componenets/content/Biography.jsx
+++ src/componenets/content/Biography.jsx
@@ -5,10 +5,9 @@ This one is the biography with the portraits of everyone who is running byte Lab
 
 //Grabbing the pictures
 import BethanPortrait from '../../assets/BethanPortrait.png';
-import candid2 from '../../assets/testimonials-2.jpg';
-import candid3 from '../../assets/testimonials-3.jpg';
-//import candid4 from '../assets/testimonials-4.jpg';
-//import candid5 from '../assets/testimonials-5.jpg';
+import ConnerPortrait from '../../assets/ConnerPortrait.JPG';
+import CharlesPortrait from '../../assets/CharlesPortrait.JPG';
+import DethanPortrait from '../../assets/DranePortrait.JPG';
 
 
 //Returning the biography website.
@@ -27,28 +26,28 @@ export default function Biography(){
                 </div>
                 <div className="col-lg-2">
                     <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                        <img className="img-fluid rounded-circle mb-3" src={candid3} alt="..." />
+                        <img className="img-fluid rounded-circle mb-3" src={CharlesPortrait} alt="..." />
                         <h5 className="text-light">Ben V.</h5>
                         <p className="text-light"><em>Eight years of computer science, from building scratch games to learning multiple languages.</em></p>
                     </div>
                 </div>
                 <div className="col-lg-2">
                     <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                        <img className="img-fluid rounded-circle mb-3" src={candid3} alt="..." />
+                        <img className="img-fluid rounded-circle mb-3" src={ConnerPortrait} alt="..." />
                         <h5 className="text-light">Conner O.</h5>
                         <p className="text-light"><em>I have 5+ years of background. I have made my own games custom automation scripts and websites.</em></p>
                     </div>
                 </div>
                 <div className="col-lg-2">
                     <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                        <img className="img-fluid rounded-circle mb-3" src={candid3} alt="..." />
+                        <img className="img-fluid rounded-circle mb-3" src={DethanPortrait} alt="..." />
                         <h5 className="text-light">Ethan D.</h5>
                         <p className="text-light"><em>I started programming in 8th grade with HTML, and now I am proficient in over 7 different coding languages.</em></p>
                     </div>
                 </div>
                 <div className="col-lg-2">
                     <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                        <img className="img-fluid rounded-circle mb-3" src={candid3} alt="..." />
+                        <img className="img-fluid rounded-circle mb-3" src={CharlesPortrait} alt="..." />
                         <h5 className="text-light">Charles D.</h5>
                         <p className="text-light"><em>Having started coding in the 6th grade, I have gained proficient experiences in multiple languages.</em></p>
                     </div>
