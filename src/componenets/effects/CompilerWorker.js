aea70811f259e5b32d88bed6502a2889a251fe93
cmoney52 <149429981+cmoney52@users.noreply.github.com>
2025-10-22 11:16:46 -0500

    Removing Comments
    Removing unneccessary comments.

--- src/componenets/effects/CompilerWorker.js
+++ src/componenets/effects/CompilerWorker.js
@@ -77,31 +77,3 @@ self.reset = function(){
   }
   return; 
 }
-  //Now we are really reading in the code and running it I promise
-  // Start the Python code, but handle input asynchronously
-  /*try{
-
-    //Have the python result for later... need to add in a GUI and not sure how yet
-    const result = await self.pyodide.runPythonAsync(code);
-    //Capturing the output and errors to show the user.
-    const stdout = await self.pyodide.runPythonAsync("sys.stdout.getvalue()");
-    //Combinig everything and outputting it unless there is nothing then sending a blank string to keep zuc happy
-    self.postMessage({ type: "result", result: stdout || "So empty here" });
-  }catch(err){
-    try{
-        //Getting the full error message
-        const stderr = await self.pyodide.runPythonAsync("sys.stderr.getvalue()");
-        console.log(stderr);
-        //Finding where the last error is and slicing to get a more compact message
-        let last_line = stderr.lastIndexOf("Error");
-        last_line = stderr.slice(last_line + 6, stderr.length);
-        //Simplifying the error message to be more user friendly and outputting it
-        let simplified = "\n---\n ERROR: " + last_line + "\n";
-        self.postMessage({type: "result", result: "Uh oh looks like you have an error! " + simplified});
-    }
-    catch(err){
-        //Broad Error catching.
-        self.postMessage({type: "result", result: "Something went wrong, please try again."});
-    }
-    }
-  }*/ 
